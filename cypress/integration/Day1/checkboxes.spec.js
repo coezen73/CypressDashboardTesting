19344ae0ee8dacefa551e2e653623f9755b8767b
cemilözen <coezen73@gmail.com>
2022-07-27 13:32:57 +0200

    file name has changed

--- cypress/integration/Day1/checkboxes.spec.js
+++ cypress/integration/Day1/checkboxes.spec.js
@@ -7,14 +7,12 @@ describe('Checkbox Testing', () => {
 
         cy.visit('http://practice.cybertekschool.com/checkboxes')
 
-        cy.viewport('samsung-note9') 
-        // -> We can change it to ipad-iphone - Samsungnote 10 or Macbook..  
+        cy.viewport('samsung-note9')    // -> We can change it to ipad-iphone - Samsungnote 10 or Macbook..  
         cy.wait(2000)
-        // => '.should' -> represented by 'assert' in Test Runner Page
-        // get the element, check it and assert it if it is checked or not;
-        cy.get('#box1').check().should('be.checked')   
-        // cy.get('#box1').check().should('not.be.checked')
-        cy.wait(2000)
+        cy.get('#box1').check().should('be.checked')  // => '.should' -> represented by 'assert' in Test Runner Page
+                                                             // get the element, check it and assert it if it is checked or not;  
+        cy.wait(2000)                                         // cy.get('#box1').check().should('not.be.checked')
+        
         cy.get('[name="checkbox2"]').click().should('not.be.checked')
 
     })
